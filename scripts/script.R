fe7eb0c588b7b55a879f2f85a8a6dbb6b2f9d53a
Benjamin Chan <chanb@ohsu.edu>
2017-08-17 12:08:35 -0700

    Modify master script

--- scripts/script.R
+++ scripts/script.R
@@ -9,9 +9,9 @@ sink("script.log")
 files <- c("header.yaml",
            "preamble.Rmd",
            "readData.Rmd",
-           "analyzeRQ1.Rmd",
-           "analyzeRQ2.Rmd",
-           "analyzeRQ3.Rmd")
+           "analyzeRQ1.Rmd")
+           # "analyzeRQ2.Rmd",
+           # "analyzeRQ3.Rmd")
 f <- file("master.Rmd", open = "w")
 for (i in 1:length(files)) {
     x <- readLines(files[i])
@@ -20,14 +20,18 @@ for (i in 1:length(files)) {
 close(f)
 library(knitr)
 library(rmarkdown)
-opts_chunk$set(echo = FALSE, fig.path = "../figures/", dpi = 300)
+opts_chunk$set(echo = FALSE,
+               fig.path = "../figures/",
+               dpi = 300,
+               messages = FALSE)
 knit("master.Rmd", output = "../docs/report.md")
-# pandoc("../docs/analysis_summary.md", format = "docx")
+pandoc("../docs/report.md", format = "docx")
 file.remove("master.Rmd")
 sink()
 
 sink("session.log")
 list(completionDateTime = Sys.time(),
      executionTime = Sys.time() - Sys.time0,
-     sessionInfo = sessionInfo())
+     sessionInfo = sessionInfo(),
+     citation = citation())
 sink()
