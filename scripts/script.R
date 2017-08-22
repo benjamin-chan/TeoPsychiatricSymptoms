1e65e0a7a7e9e06e485e2bbfd9197129400cc5f7
Benjamin Chan <chanb@ohsu.edu>
2017-08-22 15:34:00 -0700

    Update master script

--- scripts/script.R
+++ scripts/script.R
@@ -9,8 +9,8 @@ sink("script.log")
 files <- c("header.yaml",
            "preamble.Rmd",
            "readData.Rmd",
-           "analyzeRQ1.Rmd")
-           # "analyzeRQ2.Rmd",
+           "analyzeRQ1.Rmd",
+           "analyzeRQ2.Rmd")
            # "analyzeRQ3.Rmd")
 f <- file("master.Rmd", open = "w")
 for (i in 1:length(files)) {
@@ -33,5 +33,6 @@ sink("session.log")
 list(completionDateTime = Sys.time(),
      executionTime = Sys.time() - Sys.time0,
      sessionInfo = sessionInfo(),
-     citation = citation())
+     citation0 = citation(),
+     citation1 = citation("multcomp"))
 sink()
