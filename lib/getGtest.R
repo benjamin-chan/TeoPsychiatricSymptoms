5b2a97a97e1538fbb1111f83f1a123ffc3decf8c
Benjamin Chan <chanb@ohsu.edu>
2017-08-23 15:11:45 -0700

    Fix getGtest function

--- lib/getGtest.R
+++ lib/getGtest.R
@@ -2,7 +2,7 @@ getGtest <- function (modelObject, linfct = NULL) {
   require(multcomp)
   if (is.null(linfct)) {
     K <- diag(length(coef(modelObject)))[-1,]
-    rownames(K) <- names(coef(lmod))[-1]
+    rownames(K) <- names(coef(modelObject))[-1]
     linfct <- K
   }
   modelObject %>% glht(linfct = linfct) %>% summary(test = Ftest()) %>% .[["test"]]
