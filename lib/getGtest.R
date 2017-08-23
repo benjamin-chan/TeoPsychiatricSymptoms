bd6e20f072dd08c1ba90a6603777207585454c09
Benjamin Chan <chanb@ohsu.edu>
2017-08-23 12:00:48 -0700

    Create function to extract gtest object

---
+++ lib/getGtest.R
@@ -0,0 +1,9 @@
+getGtest <- function (modelObject, linfct = NULL) {
+  require(multcomp)
+  if (is.null(linfct)) {
+    K <- diag(length(coef(modelObject)))[-1,]
+    rownames(K) <- names(coef(lmod))[-1]
+    linfct <- K
+  }
+  modelObject %>% glht(linfct = linfct) %>% summary(test = Ftest()) %>% .[["test"]]
+}
