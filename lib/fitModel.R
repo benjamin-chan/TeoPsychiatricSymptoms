c6f37648b247d381d31b3bf0fa3ce8c3cb2fd441
Benjamin Chan <chanb@ohsu.edu>
2017-09-06 11:23:15 -0700

    Extend fitModel function to allow for interaction with x

--- lib/fitModel.R
+++ lib/fitModel.R
@@ -1,8 +1,11 @@
-fitModel <- function (df, y, x, covar = NULL) {
+fitModel <- function (df, y, x, covar = NULL, int = NULL) {
   if (is.null(covar)) {
     f <- formula(sprintf("%s ~ %s", y, x))
   } else {
     f <- formula(sprintf("%s ~ %s + %s", y, x, paste(covar, collapse = " + ")))
+  }
+  if (!is.null(int)) {
+    f <- update(f, sprintf("~ . + %s + %s", int, paste(x, int, sep = "*")))
   }
   M <- glm(f, data = df, family = "binomial")
   list(modelObject = M,
